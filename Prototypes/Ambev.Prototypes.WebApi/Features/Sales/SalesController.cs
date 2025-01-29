2a850f8ef7e155c67fec8593e11dab9eb19f869a
José Seraphim <curcif@live.com>
2025-01-28 23:04:28 -0300

    Initial Commit

---
+++ Prototypes/Ambev.Prototypes.WebApi/Features/Sales/SalesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ambev.Prototypes.WebApi.Features.Sales
+{
+    public class SalesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
